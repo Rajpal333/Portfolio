51936c6e4c7d31170c1d28da245a6611359e184c
Rajpal Singh <rajpalsinghpanwar304@gmail.com>
2026-05-09 17:34:54 +0530

    Connected backend and python services

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,24 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "https://portfolio-goie.onrender.com/api",
+// });
+
+// export const getSkills = () => API.get("/skills");
+// export const getProjects = () => API.get("/projects");
+// export const getContact = () => API.get("/contact");
+
 import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://portfolio-goie.onrender.com/api",
 });
 
+// Backend APIs
 export const getSkills = () => API.get("/skills");
 export const getProjects = () => API.get("/projects");
 export const getContact = () => API.get("/contact");
+
+// Python AI Graph API
+export const getGraph = () =>
+  axios.get("https://portfolio-python-service.onrender.com/graph");
