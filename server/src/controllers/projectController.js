ed626d3b5262d8915665e91c38cc23aa7172de29
Rajpal Singh <rajpalsinghpanwar304@gmail.com>
2026-05-09 18:07:59 +0530

    portfolio updated

--- server/src/controllers/projectController.js
+++ server/src/controllers/projectController.js
@@ -18,7 +18,7 @@ exports.createProject = async (req, res) => {
 // GET All Projects
 exports.getProjects = async (req, res) => {
   try {
-    const projects = await Project.find();
+    const project = await Project.find();
     res.status(200).json(projects);
   } catch (err) {
     res.status(500).json({ message: err.message });
