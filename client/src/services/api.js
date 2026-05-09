f99d616cb50db9b7d863f6ce501205c9889b7c80
Rajpal Singh <rajpalsinghpanwar304@gmail.com>
2026-05-09 17:18:10 +0530

    Connected frontend with live backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://portfolio-goie.onrender.com/api",
 });
 
 export const getSkills = () => API.get("/skills");
